00d47cf043125d10ca32ba5a917d0d56975f9422
pujie <puji@gmail.com>
2018-05-18 16:11:32 +0700

    show detail of surveys

--- www/js/appsettings.js
+++ www/js/appsettings.js
@@ -4,9 +4,8 @@ app.controller('AppsettingsCtrl', ['appconfig','$scope',function(appconfig,$scop
     port:appconfig.PORT
   };
   $scope.saveSetting = function(){
-    //app.constant('appconfig',{'SERVER':'192.168.0.117','PORT':'1946'});
-    appconfig.SERVER = $scope.project.server;//'192.168.0.118';
-    appconfig.PORT = $scope.project.port;//'1946';
+    appconfig.SERVER = $scope.project.server;
+    appconfig.PORT = $scope.project.port;
     console.log("appconfig wrotes");
   }
   console.log(appconfig);
